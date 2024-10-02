6064006e0129c8b204ac4be81d951dba210c3fe4
RishithaNaga <nagarishitha181292@gmail.com>
2024-10-02 16:00:27 +0530

    ....

--- testcases/jenkins_test.py
+++ testcases/jenkins_test.py
@@ -4,5 +4,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pageObjects.signup_and_login_page import Login
 from .conftest import pytest_addoption
-def test_one():
-    print("hello world")
+
+class Testone:
+    def test_one(self):
+        print("hello world")
